edb377d1ed38586f4878d026de5c92a3ff81ed28
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-11-21 14:47:36 +0530

    commm

---
+++ 8_NodeJs/index.js
@@ -0,0 +1,12 @@
+// const superheroes = require('superheroes');
+
+import superheroes from 'superheroes';
+
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+var mySuperhero = superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(mySuperhero);
+

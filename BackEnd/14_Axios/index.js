75576240df0fa3eba9ea8c627c85287b73516f30
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-11-26 11:10:04 +0530

    commit

--- BackEnd/14_Axios/index.js
+++ BackEnd/14_Axios/index.js
@@ -14,13 +14,36 @@ app.get('/', async (req, res) => {
     try{
         const response = await axios.get("https://bored-api.appbrewery.com/random");
         const result = response.data;
-        console.log(result);
+        const data = {apiItem: result}
+        res.render('./index', data)
     }
     catch (error){
         console.error("Failed to make API request:", error.message);
     }
 
 })
+
+app.post('/',async (req,res) => {
+    try {
+        console.log(req.body);
+        const type = req.body.type;
+        const participants = req.body.participants;
+        const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`)
+        const result = response.data
+        console.log(result);
+        const data = {
+            apiItem: result[Math.floor(Math.random() * result.length)]
+        }
+        res.render('./index', data)
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("./index", {
+          error: "No activities that match your criteria.",
+        });
+    }
+})
+
+
 
 
 app.listen(port, () => {

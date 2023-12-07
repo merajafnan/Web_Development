44e847f88e2ddf65d2603dcbf01e849dfb53387a
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-07 13:34:19 +0530

    Country traveller project aded

--- BackEnd/17_Travel_Tracker/index.js
+++ BackEnd/17_Travel_Tracker/index.js
@@ -19,23 +19,51 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", async (req, res) => {
+async function checkVisited() {
   
   const result = await db.query("select country_code from visited_countries");
   let countries = [];
-  
   result.rows.forEach((test) => {
     countries.push(test.country_code)
   });
+  return countries
+}
+
+app.get("/", async (req, res) => {
   
+  const countries = await checkVisited();
   res.render('index.ejs',{countries: countries,total: countries.length})
-  
-  console.log(result);
-  console.log(result.rows);
-  console.log(countries);
-  db.end();
+
 });
+
+app.post("/add", async(req,res) => {
+
+  const input = req.body["country"];
+
+  try{
+      const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1",[input]);
+      const countryCode = result.rows[0].country_code;
+
+      try {
+            await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[countryCode,]);
+            res.redirect("/")
+      }
+      catch (err) {
+            console.log(err);
+            const countries = await checkVisited();
+            res.render('index.ejs',{countries: countries, total: countries.length, error: 'Duplicate Entry, please try again.'})
+      }
+
+  }
+  catch (err) {
+      console.log(err);
+      const countries = await checkVisited();
+      res.render('index.ejs',{countries: countries, total: countries.length, error: 'Country Name does not exist, please try again.'})
+  }
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);

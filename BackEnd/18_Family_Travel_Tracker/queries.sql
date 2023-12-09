66265048881e702a622e32d160da2d91e2ea6333
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-09 15:54:30 +0530

    comm

--- BackEnd/18_Family_Travel_Tracker/queries.sql
+++ BackEnd/18_Family_Travel_Tracker/queries.sql
@@ -118,3 +118,29 @@ SELECT *
 FROM visited_countries
 JOIN users
 ON users.id = user_id;
+
+
+
+-- EXERCISE
+
+
+
+-- DROP TABLE IF EXISTS visited_countries, users;
+
+-- CREATE TABLE users (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name VARCHAR(45) UNIQUE NOT NULL,
+-- 	color VARCHAR(15)
+-- );
+
+-- CREATE TABLE visited_countries(
+-- 	id SERIAL PRIMARY KEY,
+-- 	country_code CHAR(2) NOT NULL,
+-- 	user_id INTEGER REFERENCES users(id)
+-- );
+
+-- INSERT INTO users (name,color) VALUES ('Meraj','teal'),('Shreen','powderblue');
+
+-- INSERT INTO visited_countries (country_code,user_id) VALUES ('FR',1),('GB',1),('CA',2),('FR',2);
+
+SELECT * FROM visited_countries JOIN users ON users.id = visited_countries.user_id;
